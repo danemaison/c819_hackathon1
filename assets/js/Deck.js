143e6eee9d5f59cf44109658b23dd9fcfefb11c7
Dane Maison <danemaison@Danes-MacBook-Pro.local>
2019-09-04 10:40:39 -0700

    added deck skeleton

--- assets/js/Deck.js
+++ assets/js/Deck.js
@@ -1,19 +1,21 @@
 class Deck{
-  constructor(cardsArray, visible, placeCardsInDeck, draw){
+  constructor(){
     this.cardsArray = [];
-    this.visible = visible;
-  //deckPoints?
-  //reorder?
-  //playerOwner?
+    // this.visible = false;
   }
   placeInDeck(cardObj){
     this.cardsArray.push(cardObj);
   }
-  draw(targetDeck){
+  draw(){
     var lastCard = this.cardsArray.pop();
     return lastCard;
   }
   shuffle(){
-
+    for(var currentIndex = this.cardsArray.length - 1; currentIndex > 0; currentIndex--){
+      var randomIndex = Math.floor(Math.random() * currentIndex);
+      var temp = this.cardsArray[currentIndex];
+      this.cardsArray[currentIndex] = this.cardsArray[randomIndex];
+      this.cardsArray[randomIndex] = temp;
+    }
   }
 }
