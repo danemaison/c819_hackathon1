060418123067e4a4080094751f589fc4081babcb
Christine Le <christinele@csu.fullerton.edu>
2019-09-04 09:27:51 -0700

    deck skeleton

---
+++ main.js
@@ -0,0 +1,12 @@
+class Deck{
+  constructor(cardsArray, visible, placeCardsInDeck, draw){
+    this.cardsArray = [];
+    this.visible = true;
+    this.placeCardsInDeck = {};
+
+    this.draw = $(this.domElement);
+  //deckPoints?
+  //reorder?
+  //playerOwner?
+
+  }
