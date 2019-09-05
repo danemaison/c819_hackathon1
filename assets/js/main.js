fac507ed46fb581b261addf8727edbaa3946616e
Dane Maison <danemaison@gmail.com>
2019-09-05 12:52:51 -0700

    added finalized cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@ var images = [
   'body4.jpg',
   'body5.jpg',
   'body6.jpg',
-  'body7.jpg',
   'body8.jpg',
   'body9.jpg',
   'head1.png',
@@ -36,49 +35,68 @@ var images = [
   'legs5.jpg',
   'legs6.jpg',
   'legs7.jpg',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
+  'baby',
 ]
+
+var gameBoard = new Board();
 
 function loadCards(){
   for(var i = 0; i < images.length; i++){
+    var card = null;
+    var path = 'assets/images/' + images[i];
+    if(images[i].includes('body')){
+      card = new Card(gameBoard.drawDeck, 'body', 1, path, null);
+    }
+    else if(images[i].includes('head')){
+      card = new Card(gameBoard.drawDeck, 'head', 2, path, null);
+    }
+    else if (images[i].includes('leftArm')) {
+      card = new Card(gameBoard.drawDeck, 'leftArm', 1, path, null);
+    }
+    else if (images[i].includes('rightArm')) {
+      card = new Card(gameBoard.drawDeck, 'rightArm', 1, path, null);
+    }
+    else if (images[i].includes('legs')) {
+      card = new Card(gameBoard.drawDeck, 'legs', 2, path, null);
+    }
+    else if (images[i].includes('baby')) {
+      card = new Card(gameBoard.drawDeck, 'baby', Math.floor(Math.random() * 3));
+    }
+    gameBoard.drawDeck.placeInDeck(card);
   }
 }
 
-function initializeApp(){
-  var gameBoard = new Board();
-  var colors = ['red', 'green', 'blue'];
-  for (var i = 0; i < 30; i++){
-    var tempBaby = new Card(gameBoard.drawDeck, "baby", Math.floor(Math.random() * 3), "baby", "http://placecorgi.com/200/500", colors[Math.floor(Math.random() * 3)]);
-    var tempHead = new Card(gameBoard.drawDeck, "head", 2, "head", "http://placecorgi.com/200/500", colors[Math.floor(Math.random() * 3)]);
-    var tempLeg = new Card(gameBoard.drawDeck, "legs", Math.floor(Math.random() * 2) + 1, "legs", "http://placecorgi.com/200/500", "");
-    var tempRight = new Card(gameBoard.drawDeck, "rightArm", 1, "rightArm", "http://placecorgi.com/200/500", "");
-    var tempLeft = new Card(gameBoard.drawDeck, "leftArm", 1, "leftArm", "http://placecorgi.com/200/500", "");
-    var tempBody = new Card(gameBoard.drawDeck, "body", 1, "body", "http://placecorgi.com/200/500", "");
-    gameBoard.drawDeck.placeInDeck(tempBaby);
-    gameBoard.drawDeck.placeInDeck(tempHead);
-    gameBoard.drawDeck.placeInDeck(tempLeg);
-    gameBoard.drawDeck.placeInDeck(tempRight);
-    gameBoard.drawDeck.placeInDeck(tempLeft);
-    gameBoard.drawDeck.placeInDeck(tempBody);
-  }
-  // randomize deck
-  // gameBoard.drawDeck.shuffle();
-  gameBoard.drawDeck.shuffle()
-
+function initializePlayerDecks(){
   // for each player in the game, pass five cards out.
-  for (var player of gameBoard.players){
-    for (var i = 0; i < 5; i++){
+  for (var player of gameBoard.players) {
+    for (var i = 0; i < 5; i++) {
       var handout = gameBoard.drawDeck.draw()
       player.deck.placeInDeck(handout);
     }
   }
+}
 
-  // gameBoard.startGame(); // go to player one, set actions remaining to 3
+function initializeApp(){
+  loadCards();
+  gameBoard.drawDeck.shuffle()
+  initializePlayerDecks();
   gameBoard.generateDom();
   gameBoard.players[0].render();
-  // while(!gameBoard.gameOver()){
-  //   gameBoard.takeTurn();
-  // }
-  // begin the game...board ne
-  // console.log(player)
-  // player.render();
 }
