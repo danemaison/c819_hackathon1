1890a628efb490ce5135785f0d7873784b6bdae3
Dane Maison <danemaison@gmail.com>
2019-09-05 12:28:34 -0700

    added images and fixed timeout time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,52 @@
 $(document).ready(initializeApp)
 
+var images = [
+  'body1.png',
+  'body2.jpg',
+  'body3.jpg',
+  'body4.jpg',
+  'body5.jpg',
+  'body6.jpg',
+  'body7.jpg',
+  'body8.jpg',
+  'body9.jpg',
+  'head1.png',
+  'head2.jpg',
+  'head3.png',
+  'head4.jpg',
+  'head5.jpg',
+  'head6.jpg',
+  'head7.jpg',
+  'head9.jpg',
+  'head10.jpg',
+  'head11.jpg',
+  'head12.jpg',
+  'leftArm1.jpg',
+  'leftArm2.jpg',
+  'leftArm3.jpg',
+  'leftArm4.jpg',
+  'rightArm1.jpg',
+  'rightArm2.jpg',
+  'rightArm3.jpg',
+  'rightArm4.jpg',
+  'legs1.png',
+  'legs2.jpg',
+  'legs3.jpg',
+  'legs4.jpg',
+  'legs5.jpg',
+  'legs6.jpg',
+  'legs7.jpg',
+]
+
+function loadCards(){
+  for(var i = 0; i < images.length; i++){
+  }
+}
+
 function initializeApp(){
   var gameBoard = new Board();
   var colors = ['red', 'green', 'blue'];
   for (var i = 0; i < 30; i++){
-    // babies and heads are given colors
     var tempBaby = new Card(gameBoard.drawDeck, "baby", Math.floor(Math.random() * 3), "baby", "http://placecorgi.com/200/500", colors[Math.floor(Math.random() * 3)]);
     var tempHead = new Card(gameBoard.drawDeck, "head", 2, "head", "http://placecorgi.com/200/500", colors[Math.floor(Math.random() * 3)]);
     var tempLeg = new Card(gameBoard.drawDeck, "legs", Math.floor(Math.random() * 2) + 1, "legs", "http://placecorgi.com/200/500", "");
